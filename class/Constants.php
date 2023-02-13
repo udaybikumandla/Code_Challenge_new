4f338cbc2129aab7590c0354311bc0a5dcf9cc7f
udaybikumandla <122776851+udaybikumandla@users.noreply.github.com>
2023-02-13 18:27:52 +0530

    Update Constants.php

--- class/Constants.php
+++ class/Constants.php
@@ -2,26 +2,10 @@
 namespace AllConstants;
 
 Class Constants{
-
-    protected const ACCOUNT_TYPE_CHECKING = 'Checking';
-    protected const ACCOUNT_TYPE_INVESTMENT = 'investment';
-    protected const SUB_ACCOUNT_TYPE_INDIVIDUAL ='individual';
-    protected const SUB_ACCOUNT_TYPE_CORPORATE ='corporate';
     
-    public function AccounttypeChecking() {
-        return self:: ACCOUNT_TYPE_CHECKING;
-    } 
-
-    public function AccounttypeInvestment() {
-        return self:: ACCOUNT_TYPE_INVESTMENT;
-    } 
-
-    public function SubAccounttypeIndividual(){
-        return self:: SUB_ACCOUNT_TYPE_INDIVIDUAL;
-    }
-
-    public function subAccounttypecorporate(){
-        return self:: SUB_ACCOUNT_TYPE_CORPORATE;
-    }
-
+    public const ACCOUNT_TYPE_CHECKING = 'Checking';
+    public const ACCOUNT_TYPE_INVESTMENT = 'investment';
+    public const SUB_ACCOUNT_TYPE_INDIVIDUAL ='individual';
+    public const SUB_ACCOUNT_TYPE_CORPORATE ='corporate';
 }
+?>
