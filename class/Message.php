39f689821bc9f18dd6ff0d041154017fdf18f876
udaybikumandla <122776851+udaybikumandla@users.noreply.github.com>
2023-02-13 18:27:17 +0530

    Update Message.php

--- class/Message.php
+++ class/Message.php
@@ -1,174 +1,41 @@
 <?php
 namespace Messages;
 
-class Message{
-
-    public function ApiTypeInvaliedMsg()
-    {
-        return "API type invalied.";
-    }
-    
-    public function AccountOwnerNameExitsMsg(){
-        return "Account owner name already exits.";
-    }
-    
-    public function AccountNumberExitsMsg(){
-        return "Account number already exits.";
-    }
-    public function MobileNumberExitsMsg(){
-        return "Mobile Number already exits.";
-    }
-
-    public function AccountSuccessMsg()
-    {
-        return "Account created successfully";
-    }
-    
-    public function NoAccount()
-    {
-        return "There is no account.";
-    }
-    
-    public function AccountUpdateSuccess()
-    {
-        return "Account Updated Successfully.";
-    }
-    
-    public function BankOwenerList()
-    {
-        return "Bank Account Owner List.";
-    }
-    
-    public function NoAccounts()
-    {
-        return "There is no accouts list.";
-    }
-    
-    public function BankOwenerDetails()
-    {
-        return "Bank account owner details.";
-    }
-    
-    public function DepositeSuccess()
-    {
-        return "Amount Deposited successfully.";
-    }
-
-    public function InsufficientMsg()
-    {
-        return "You have insufficient balance.";
-    }
-
-    public function IndividualWithdrawlLimitMSG()
-    {
-        return "Individual accounts have a withdrawal limits of 500 dollers";
-    }
-
-    public function WithdrawalScussessMSG()
-    {
-        return "Amount withdrawal successfully.";
-    }
-
-    public function TransactionTypeInvaliedMSG()
-    {
-        return "Transaction type invalied.";
-    }
-
-    public function FromAccountNotExitMsg()
-    {
-        return "From account not exits or not activated.";
-    }
-
-    public function ToAccountNotExitMsg()
-    {
-        return "To account not exits or not activated.";
-    }
-
-    public function TrasferredSuccessMsg()
-    {
-        return "Amount transferred successfully.";
-    }
-
-    public function OwnerNotActiveMsg()
-    {
-        return "This Accoout owner id in-active.";
-    }
-
-    public function OwnerNameWithType()
-    {
-        return "Bank account owner name with account types.";
-    }
-
-    public function AccountTypeMsg()
-    {
-        return "Please provide bank account type.";
-    }
-
-    public function subAccountTypeMsg()
-    {
-        return "Please provide bank sub account type.";
-    }
-
-    public function OwnerNameMSg()
-    {
-        return "Please provide owner name.";
-    }
-
-    public function OwnerNameValidationMsg()
-    {
-        return "Owner name should be only alphabets and whitespace allowed.";
-    }
-
-    public function  MobileNumberMsg()
-    {
-        return "Please provide mobile number.";
-    }
-
-    public function AddressMsg()
-    {
-        return "Please provide address.";
-    }
-
-    public function OwnerIdMsg()
-    {
-        return "Please provide ownerId.";
-    }
-
-    public function transactionTypeMsg()
-    {
-         return "Please provide transactionType.";
-    }
-    public function transactionTypInvaliedMsg()
-    {
-        return "TransactionType invalied.";
-    }
-    public function ammountMsg()
-    {
-        return "Please provide amount.";
-    }
-    public function transactionModeMsg()
-    {
-        return "Please provide tarnsactionMode.";
-    }
-
-    public function accountRemovedSuccessMsg()
-    {
-        return "Owner account has been removed.";
-    }
-
-    public function  toOwnerIdMsg()
-    {
-        return "Please provide toOwnerid.";
-    }
-
-    public function toAccountNumber()
-    {
-        return "Please provide toaccountno.";
-    }
-
-    public function accountNumberMsg()
-    {
-      return "Please provide account no.";   
-    }
+class Message
+{
+    public const API_TYPE_INVALIED ='API type invalied.';
+    public const ACCOUNT_OWNER_NAME_EXITS ='Account owner name already exits.';
+    public const ACCOUNT_NUMBER_EXITS = 'Account number already exits.';
+    public const MOBILE_NUMBER_EXITS = 'Mobile Number already exits';
+    public const ACCOUNT_SUCCESS_MSG ='Account created successfull.';
+    public const ACCOUNT_TYPE_MSG = 'Please provide bank account type.';
+    public const SUB_ACCOUNT_TYPE_MSG ='Please provide bank sub account type.';
+    public const OWNER_NAME_MSG = 'Please provide owner name';
+    public const OWNER_NAME_VALIDATION_MSG = "Owner name should be only alphabets and whitespace allowed.";
+    public const MOBILE_NUMBER_MSG = 'Please provide mobile number.';
+    public const ADDRESS_MSG = 'Please provide address.';
+    public const NO_ACCOUNT_MSG = 'There is no account.';
+    public const ACCOUNT_UPDATE_SUCCESS ='Account Updated Successfully.';
+    public const BANK_OWNER_LIST = 'Bank Account Owner List.';
+    public const NO_ACCOUNTS_MSG = 'There is no accouts list.';
+    public const OWNER_ID_MSG = 'Please provide ownerId.';
+    public const ACCOUNT_NUMBER_MSG = 'Please provide account no.';
+    public const BANK_OWNER_DETAILS = 'Bank account owner details.';
+    public const DEPOSITE_SUCCESS_MSG = 'Amount Deposited successfully.';
+    public const INSUFFICIENT_MSG = 'You have insufficient balance.';
+    public const INDIVIDUAL_WITHDRAW_LIMIT_MSG = 'Individual accounts have a withdrawal limits of 500 dollers.';
+    public const WITHDRAWAL_SUCCESS_MSG = 'Amount withdrawal successfully.';
+    public const TRANSACTION_TYPE_INVALIED = 'Transaction type invalied.';
+    public const FROM_ACCOUNT_NOT_EXITS_MSG = 'From account not exits or not activated.';
+    public const TO_ACCOUNT_NOT_EXITS_MSG = 'To account not exits or not activated.';
+    public const TRANSFER_SUCCSS_MSG ='Amount transferred successfully.';
+    public const OWNER_NOT_ACTIVE_MSG = 'This Accoout owner id in-active.';
+    public const OWNER_NAME_WITH_TYPE ='Bank account owner name with account types.';
+    public const TRANSACTION_TYPE_MSG = 'Please provide transactionType.';
+    public const AMMOUNT_MSG = 'Please provide amount.';
+    public const TRANSACTION_MODE_MSG = 'Please provide tarnsactionMode.';
+    public const ACCOUNT_REMOVED_SUCCESS_MSG ='Owner account has been removed.';
+    public const TO_OWNER_ID_MSG ='Please provide toOwnerid.';
+    public const TO_ACCOUNT_NUMBER = 'Please provide toaccountno.';
 }
 ?>
